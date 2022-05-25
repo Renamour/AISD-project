685ff156550d37606d95fd13fb1ad51c16551c98
Renamour <66073959+Renamour@users.noreply.github.com>
2022-05-25 22:14:10 +0400

    Update main.py

--- lab2-3/main.py
+++ lab2-3/main.py
@@ -1,43 +1,29 @@
-import os
-import  re
+import re
 import time
-buffer_len = 1  # размер буфера чтения
-work_buffer = ""  # рабочий буфер
-engl_flag = False  # флаг англ. текста
+max_buffer_len = 1000  # максимальный размер рабочего буфера
 try:
-    with open("test.txt", "r") as file:  # открываем файл
-        print("\n-----Результат работы программы-----\n")
-        buffer = file.read(buffer_len)  # читаем первый блок
+    filePath = input('Введите путь к файлу: ')  # Получаем путь к файлу.
+    with open(filePath, "r", encoding="utf-8") as f:  # открываем файл
+        buffer = f.read(max_buffer_len)  # читаем первый блок
         if not buffer:  # если файл пустой
-            print(
-                "\nФайл text.txt в директории проекта пустой.\nДобавьте не пустой файл в директорию или переименуйте существующий *.txt файл.")
-        while buffer:  # пока файл не пустой
-            if buffer >= 'A' and buffer <= 'Z':  # обрабатываем текущий блок
-                engl_flag = True
-                work_buffer += buffer.lower() # переводим в нижний регистр символы
-            else:
-                work_buffer += buffer
-                if buffer >= 'a' and buffer <= 'z':
-                    engl_flag = True
-            if buffer.find(".") >= 0 or buffer.find("!") >= 0 or buffer.find("?") >= 0:  # если символ- окончание предложения
-                if engl_flag:
-                 print(work_buffer)
-                 s = work_buffer
-                 s = re.sub(r'[^\w\s]', '', s) #  удаляем знаки препинания из текста
-                 s = s.split() # разбиваем строку на слова
-                 maxi = max(s, key=len) # находим слово с наибольшим количеством символов
-                 count = len(maxi) # подсчет количества символов в слове
-                 pozition=work_buffer.find(maxi) # поиск начального индекса длинного слова
-                engl_flag = False
-                work_buffer = ""
-            buffer = file.read(buffer_len)
-        print("Время выполнения: " + str(time.process_time()))
-        print("Самое длинное слово =", maxi)
-        print("Длина длинной последовательности = ", count)
-        print("Начальная позиция или индекс самого длинного слова:", pozition)
+            print("Файл пустой.\nОтредактируйте файл или добавьте не пустой файл text.txt.\n")
+            print("Время работы: ",time.process_time(), "секунд")
+        else:
+            while buffer:
+                s = buffer
+                print(s)   #вывод текста из файла
+                s = re.sub(r'[^\w\s]', ' ', s)  # удаляем знаки препинания из текста
+                words = dict()
+                for word in s.split(" "):  # разбиваем строку на слова
+                    words[len(word)] = word
+                biggestWord = words[max(words)] # находим слово с наибольшим количеством английских символов
+                count = len(biggestWord)  # подсчет количества символов в слове
+                pozition = buffer.find(biggestWord)  # поиск начального индекса длинного слова
+                buffer = f.read(max_buffer_len)   # читаем очередной блок
+            print("Самое длинное слово =", biggestWord)
+            print("Длина cамого длинного слова в тексте = ", count, "cимволов")
+            print("Начальная позиция или первый индекс самого длинного слова:", pozition)
+            print("Время работы: ", time.process_time(), "секунд")
 except FileNotFoundError:
-    print(
-        "\nФайл text.txt в директории проекта не обнаружен.\nДобавьте файл в директорию или переименуйте существующий *.txt файл.")
-
-
-
+    print("Файл text.txt в директории проекта не обнаружен.\nДобавьте файл в директорию или переименуйте существующий *.txt файл.")
+    print("Время работы программы: ", time.process_time(), "секунд")
