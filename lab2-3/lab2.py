1a005a59160c8d9d28c987f44882c9bcd2ef6a1b
Renamour <66073959+Renamour@users.noreply.github.com>
2022-05-28 20:52:02 +0400

    Update lab2.py

--- lab2-3/lab2.py
+++ lab2-3/lab2.py
@@ -19,25 +19,25 @@ try:
                 if buffer >= 'A' and buffer <= 'Z':
                     engl_flag = True
             if buffer.find(".") >= 0 or buffer.find("!") >= 0 or buffer.find("?") >= 0:  # Если символ- окончание предложения
+                print(work_buffer)
                 if engl_flag:  # Если в предложении был английский текст
-                    print(work_buffer)  # Печатаем предложение и готовим новый цикл
                     s = work_buffer
-                    s = re.sub(r'[^\w\s]', '', s)  # удаляем знаки препинания из текста
+                    s= re.sub(r'[^\w\s]+|[\d]+', r'', s).strip()
                     words = dict()
                     for word in s.split(" "):  # разбиваем строку на слова
                         words[len(word)] = word
                     biggestWord = words[max(words)]  # находим слово с наибольшим количеством английских символов
                     count = len(words[max(words)])  # подсчет количества символов в слове
                     pozition = work_buffer.find(biggestWord)  # поиск начального индекса длинного слова
+                    print("Самое длинное слово =", biggestWord)
+                    print("Длина cамого длинного слова в тексте = ", count, "cимволов")
+                    print("Начальная позиция или первый индекс самого длинного слова:", pozition)
+                else :
+                    print("Нет латинских символов.")
                     engl_flag = False
                 work_buffer = ""
-                print("Самое длинное слово =", biggestWord)
-                print("Длина cамого длинного слова в тексте = ", count, "cимволов")
-                print("Начальная позиция или первый индекс самого длинного слова:", pozition)
             buffer = file.read(buffer_len)  # читаем очередной блок
-
         if len(work_buffer) > 0:  # Если буфер переполнен и нет окончания предложения
             print("\nХвост файла text.txt не содержит знаков окончания предложения \nОткорректируйте файл text.txt в директории или переименуйте существующий *.txt файл.")
-
 except FileNotFoundError:
     print("\nФайл text.txt в директории проекта не обнаружен.\nДобавьте файл в директорию или переименуйте существующий *.txt файл.")
