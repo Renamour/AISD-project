dab6aa2285c053f8c54150c86d5d303b745d022c
Renamour <66073959+Renamour@users.noreply.github.com>
2022-05-28 13:20:14 +0400

    Update main.py

--- lab2-3/main.py
+++ lab2-3/main.py
@@ -5,7 +5,7 @@ buffer_len = 1  # размер буфера чтения
 work_buffer = ""  # рабочий буфер
 engl_flag = False  # флаг англ. текста
 try:
-    with open("text.txt", "r") as file:  # открываем файл
+    with open("text1.txt", "r") as file:  # открываем файл
         print("\n-----Результат работы программы-----\n")
         buffer = file.read(buffer_len)  # читаем первый блок
         if not buffer:  # если файл пустой
@@ -24,10 +24,10 @@ try:
                     s = work_buffer
                     s = re.sub(r'[^\w\s]', '', s)  # удаляем знаки препинания из текста
                     words = dict()
-                    for word in s.split(" "): # разбиваем строку на слова
+                    for word in s.split(" "):  # разбиваем строку на слова
                         words[len(word)] = word
-                    biggestWord = words[max(words)] # находим слово с наибольшим количеством английских символов
-                    count = len(biggestWord)  # подсчет количества символов в слове
+                    biggestWord = words[max(words)]  # находим слово с наибольшим количеством английских символов
+                    count = len(words[max(words)])  # подсчет количества символов в слове
                     pozition = work_buffer.find(biggestWord)  # поиск начального индекса длинного слова
                     engl_flag = False
                 work_buffer = ""
@@ -42,5 +42,4 @@ try:
 
 except FileNotFoundError:
     print(
-        "\nФайл text.txt в директории проекта не обнаружен.\nДобавьте файл в директорию или переименуйте существующий *.txt файл.")
-
+        "\nФайл text.txt в директории проекта
