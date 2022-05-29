de3643e33010e097b5a60be770c0395df9237378
Renamour <66073959+Renamour@users.noreply.github.com>
2022-05-29 16:57:46 +0400

    Update lab2.py

--- lab2-3/lab2.py
+++ lab2-3/lab2.py
@@ -3,10 +3,12 @@ import re
 max_buffer_len = 100  # максимальный размер рабочего буфера
 buffer_len = 1  # размер буфера чтения
 work_buffer = ""  # рабочий буфер
-engl_flag = False  # флаг англ. текста
+engl_flag = False  # флаг англ. текста'
+
 try:
-    with open("text1.txt", "r") as file:  # открываем файл
+    with open("text.txt", "r") as file:  # открываем файл
         print("\n-----Результат работы программы-----\n")
+
         buffer = file.read(buffer_len)  # читаем первый блок
         if not buffer:  # если файл пустой
             print("\nФайл text.txt в директории проекта пустой.\nДобавьте не пустой файл в директорию или переименуйте существующий *.txt файл.")
@@ -19,19 +21,22 @@ try:
                 if buffer >= 'A' and buffer <= 'Z':
                     engl_flag = True
             if len(work_buffer) >= max_buffer_len and buffer.find(" ") >= 0 or buffer.find(".")>= 0 or buffer.find("!") >= 0 or buffer.find("?") >= 0:
-                print(work_buffer)
+                print("\n-----Текст из файла-----\n ",work_buffer)
                 if engl_flag:  # Если в предложении был английский текст
                     s = work_buffer
                     s= re.sub(r'[^\w\s]+|[\d]+', r'', s).strip()
-                    words = dict()
-                    for word in s.split(" "):  # разбиваем строку на слова
-                        words[len(word)] = word
-                    biggestWord = words[max(words)]  # находим слово с наибольшим количеством английских символов
-                    count = len(words[max(words)])  # подсчет количества символов в слове
-                    pozition = work_buffer.find(biggestWord)  # поиск начального индекса длинного слова
-                    print("Самое длинное слово =", biggestWord)
-                    print("Длина cамого длинного слова в тексте = ", count, "cимволов")
-                    print("Начальная позиция самого длинного слова:", pozition+1)
+                    r = " ".join(s.split())
+                    print("Обработка и вывод из текста английскую последовательность слов = ", r)
+                    r = r.replace(" ", "")
+                    k = s.split()
+                    res = len(k)
+                    print ("Количество слов в последовательности = ",res)
+
+                    l = max(s.split(), key=len)
+                    for i in s.split():
+                        if len(i) == len(l):
+                            qw = r.find(i)
+                            print (f"Длинное слово в анг. последовательности = {i}, его количество символов =  {len(i)} и начальная позиция буквы в последовательности = {qw+1}, (считает только буквы без учета пробела, цифр, знаков препинания и др.)")
                 else :
                     engl_flag = False
                 work_buffer = ""
